11b81cdb59d237a5c7b79ef034cc557456af6034
TyphonBak <andrade.czr@gmail.com>
2020-01-23 11:03:49 -0300

    update app exec

--- app/__init__.py
+++ app/__init__.py
@@ -4,54 +4,58 @@ from flask_sqlalchemy import SQLAlchemy
 
 from app.services.imagem_manage import busca_texto_em_imagem
 
-app = Flask(__name__)
-CORS(app)
+def create_app():
 
-from os import environ
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
+    app = Flask(__name__)
+    CORS(app)
 
-db = SQLAlchemy(app)
+    from os import environ
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
 
-from app.services.pessoa_manage import listar as listar_pessoa, mostrar_presentes, marca_presenca
-# Paginas
+    db = SQLAlchemy(app)
 
-@app.route('/')
-def index():
-    context = {
-        'titulo': 'Entrada'
-    }
-    return render_template('index.html', context=context)
+    from app.services.pessoa_manage import listar as listar_pessoa, mostrar_presentes, marca_presenca
+    # Paginas
 
-@app.route('/presentes')
-def lista_presentes():
-    lista_de_presenca = listar_pessoa()
-    context = {
-        'titulo': 'Lista de Presentes',
-        'lista_de_pessoas': lista_de_presenca
-    }
-    return render_template('lista.html', context=context)
+    @app.route('/')
+    def index():
+        context = {
+            'titulo': 'Entrada'
+        }
+        return render_template('index.html', context=context)
 
-@app.route('/confirmados')
-def mostra_confirmados():
-    lista_de_presentes = mostrar_presentes()
-    context = {
-        'titulo': 'Lista de Confirmados',
-        'lista_de_pessoas': lista_de_presentes
-    }
-    return render_template('lista.html', context=context)
+    @app.route('/presentes')
+    def lista_presentes():
+        lista_de_presenca = listar_pessoa()
+        context = {
+            'titulo': 'Lista de Presentes',
+            'lista_de_pessoas': lista_de_presenca
+        }
+        return render_template('lista.html', context=context)
 
-# Funções
+    @app.route('/confirmados')
+    def mostra_confirmados():
+        lista_de_presentes = mostrar_presentes()
+        context = {
+            'titulo': 'Lista de Confirmados',
+            'lista_de_pessoas': lista_de_presentes
+        }
+        return render_template('lista.html', context=context)
 
-@app.route('/salvar-imagem', methods=['POST'])
-def salvar():
-    caminho_da_imagem = request.form['link_imagem']
-    nome = busca_texto_em_imagem(caminho_da_imagem)
-    code, resp = marca_presenca({'nome': nome})
-    return jsonify({"mensagem": nome}), 200
+    # Funções
 
-@app.route('/reseta/<segredo>')
-def reseta_presencas(segredo):
-    if segredo == 'todos':        
-        return redirect(url_for('index'))
-    abort(404)
+    @app.route('/salvar-imagem', methods=['POST'])
+    def salvar():
+        caminho_da_imagem = request.form['link_imagem']
+        nome = busca_texto_em_imagem(caminho_da_imagem)
+        code, resp = marca_presenca({'nome': nome})
+        return jsonify({"mensagem": nome}), 200
+
+    @app.route('/reseta/<segredo>')
+    def reseta_presencas(segredo):
+        if segredo == 'todos':        
+            return redirect(url_for('index'))
+        abort(404)
+
+    return app
